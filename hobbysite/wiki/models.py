52abbbf03469d09bf7163998953c6b4c395528aa
Zandalee <zandalee.labrador@student.ateneo.edu>
2025-03-05 19:17:58 +0800

    created necessary models

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory,
+                                on_delete=models.SET_NULL,
+                                null=True,
+                                related_name = 'category'
+    )
+    entry = models.TextField(null=True)
+    created_on = models.DateTimeField(auto_now_add=True)  # Set only once on creation
+    updated_on = models.DateTimeField(auto_now=True)  # Updates on every save
+
+    class Meta:
+        ordering = ['-created_on']  # Sort articles by created_on in descending order
+
+    def __str__(self):
+        return self.title
+    
+    def get_absoulute_url(self):
+        return reverse('wiki:article_detail', args=[self.pk])
