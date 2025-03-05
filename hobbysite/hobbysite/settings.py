868f06fdf0bb56338c00705aad5e094ff0310ec1
Zandalee <zandalee.labrador@student.ateneo.edu>
2025-03-06 00:13:10 +0800

    fixed levels in directory

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'hobbysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates\wiki')],
+        'DIRS': [os.path.join(BASE_DIR, 'wiki/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
