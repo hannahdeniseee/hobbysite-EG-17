ee08c2686e58548a967d8bdeba2d715fbdb84bf7
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-05 13:50:29 +0800

    Created models for Commission and Comment

---
+++ hobbysite/commissions/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('commissions:commission-detail', args=[str(self.pk)])
+
+
+
+class Comment(models.Model):
+    commission = models.ForeignKey(
+        Commission,
+        on_delete=models.CASCADE,
+    )
+    entry = models.TextField(null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)  
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
