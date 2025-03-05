e4a4212b3aff54b4d40ef0421c0075364d5228bd
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-05 13:49:46 +0800

    Created commissions app

---
+++ hobbysite/commissions/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path('commissions/list'),
+]
+
+
+app_name = 'commissions'
---
+++ hobbysite/commissions/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+# Create your views here.
