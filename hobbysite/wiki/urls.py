a676eb84e194fe2a0dec8bbd2827f9870510081b
Zandalee <zandalee.labrador@student.ateneo.edu>
2025-03-05 19:28:10 +0800

    fixed views and urls

---
+++ hobbysite/wiki/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ArticleListView, ArticleDetailView
+
+
+urlpatterns = [
+    path('wiki/articles', ArticleListView.as_view(), name='articles'),
+    path('wiki/article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+]
+
+app_name = 'wiki'
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Article
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'wiki/articles.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'wiki/article.html'
