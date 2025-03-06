c7c307d7a8852aecf06e55dca3118db92acd2e53
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-03-06 22:44:52 +0800

    added categories in list view

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Article
+from .models import Article, ArticleCategory
 
 
 class ArticleListView(ListView):
     model = Article
     template_name = 'wiki/articles.html'
     context_object_name = 'articles'
+    extra_context = { 
+        "articlecategories": ArticleCategory.objects.all()  
+    }
 
 
 class ArticleDetailView(DetailView):
