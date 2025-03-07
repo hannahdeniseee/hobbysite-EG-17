ae20a9c9162dc45d8036276afd6d43dac80237ff
Kimberly Sioco <kimberly040105@gmail.com>
2025-03-07 14:14:20 +0800

    Edited admin panels

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -2,21 +2,22 @@ from django.contrib import admin
 from .models import Post, PostCategory
 
 
-class TaskInline(admin.TabularInline):
+class PostInline(admin.TabularInline):
     model = Post
 
 
-class TaskGroupAdmin(admin.ModelAdmin):
+class PostCategoryAdmin(admin.ModelAdmin):
     model = PostCategory
-    inlines = [TaskInline, ]
+    list_display = ('name', 'description')
+    inlines = [PostInline, ]
 
 
-class TaskAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     model = Post
     search_fields = ('title', 'category')
     list_display = ('title', 'category', 'created_on', 'updated_on')
     list_filter = ('category', 'created_on', 'updated_on')
 
 
-admin.site.register(PostCategory, TaskGroupAdmin)
-admin.site.register(Post, TaskAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Post, PostAdmin)
