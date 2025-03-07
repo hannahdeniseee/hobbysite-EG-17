5035b7c48707307cfba1b9190003101f788e4a2a
hannahdeniseee <101623567+hannahdeniseee@users.noreply.github.com>
2025-03-07 23:23:22 +0800

    Update urls.py

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import MerchListView, MerchDetailView
 
 urlpatterns = [
-    path('items/', MerchListView.as_view(), name="merch-list"),
+    path('items', MerchListView.as_view(), name="merch-list"),
     path('item/<int:pk>', MerchDetailView.as_view(), name='merch-detail')
 ]
 # This might be needed, depending on your Django version
