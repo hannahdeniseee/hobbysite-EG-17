ab2125a2cef6a5b21d3c368fc64fc7765e57b546
hannahdeniseee <101623567+hannahdeniseee@users.noreply.github.com>
2025-03-07 18:23:43 +0800

    Fixed urls.py

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -3,7 +3,7 @@ from .views import MerchListView, MerchDetailView
 
 urlpatterns = [
     path('items/', MerchListView.as_view(), name="merch-list"),
-    path('<int:pk>/detail/', MerchDetailView.as_view(), name='merch-detail')
+    path('item/<int:pk>', MerchDetailView.as_view(), name='merch-detail')
 ]
 # This might be needed, depending on your Django version
 app_name = "merchstore"
