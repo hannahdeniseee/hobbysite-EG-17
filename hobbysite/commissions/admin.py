0ada5fe9c9887a09f40f128c5329a1f7ac5750ee
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-07 22:01:56 +0800

    Edited admin panel

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -7,9 +7,17 @@ class CommentInline(admin.TabularInline):
     model = Comment
 
 
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
+    search_fields = ('title',)
+    list_display = ('title', 'description', 'created_on', 'updated_on')
+    list_filter = ('created_on', 'updated_on')
     inlines = [CommentInline,]
 
 
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Commission, CommissionAdmin)
