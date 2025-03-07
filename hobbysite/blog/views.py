101aa803f481bd936759bb3f1d4dc28e7fc55f24
erennuevo <lib.nuevo@student.ateneo.edu>
2025-03-07 23:37:32 +0800

    Updated views.py to match pep8 standards

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -5,6 +5,7 @@ Django views for handling article-related pages.
 from django.shortcuts import render
 from .models import Article, ArticleCategory
 
+
 def article_list(request):
     """
     View function for displaying the list view of blog.
@@ -18,11 +19,12 @@ def article_list(request):
     }
     return render(request, 'article_list.html', ctx)
 
+
 def article_detail(request, id):
     """
     View function for displaying the detail view of each article.
     """
     ctx = {
-        'article': Article.objects.get(id=id) 
+        'article': Article.objects.get(id=id)
     }
     return render(request, 'article_detail.html', ctx)
