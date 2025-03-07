85e74a6668ad1e0fc4ee4bb3ae7fd6ab2294c8d4
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-07 23:18:43 +0800

    Added class meta for commission

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -9,6 +9,9 @@ class Commission(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['created_on']
+
     def __str__(self):
         return self.title
     
