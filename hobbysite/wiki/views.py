c300579cd11995fd30e6490576eb5cf213f63cbe
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-05-09 13:55:02 +0800

    created create view

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 from .models import Article, ArticleCategory
+from .forms import ArticleForm
 
 
 class ArticleListView(ListView):
@@ -15,3 +20,13 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'wiki/article.html'
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = "article_add.html"
+    form_class = ArticleForm
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
