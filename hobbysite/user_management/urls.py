e6e4e48e594544a06567139b3b62b1b216a5d157
Kimberly Sioco <kimberly040105@gmail.com>
2025-05-09 18:53:36 +0800

    Fixed dashboard link

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import DashboardView
 
 urlpatterns = [
-    path('dashboard/', DashboardView.as_view(), name='dashboard')
+    path('dashboard', DashboardView.as_view(), name='dashboard')
 ]
 
 app_name = "user_management"
