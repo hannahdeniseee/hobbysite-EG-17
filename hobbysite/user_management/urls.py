4fa0754e2cba22aa2b4f166f008b993657ca39a0
hannahdenisebacomo@gmail.com <hannahdenisebacomo@gmail.com>
2025-05-10 13:41:38 +0800

    Added extends base.html to forum and fixed merchstore

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path
 from .views import DashboardView
-
-urlpatterns = [
-    path('dashboard', DashboardView.as_view(), name='dashboard')
-]
-
-app_name = "user_management"
-from django.urls import path
 from .views import ProfileUpdateView
 
-app_name = 'user_management'
+app_name = "user_management"
 
 urlpatterns = [
+    path('dashboard', DashboardView.as_view(), name='dashboard'),
     path('profile/<str:username>/', ProfileUpdateView.as_view(), 
-         name='profile-update'),
+         name='profile-update')
 ]
+
