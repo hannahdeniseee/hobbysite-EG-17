f75c716b2539408b61e9d95b5df724af228ef092
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-05-12 18:08:13 +0800

    added create and update urls

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 
 urlpatterns = [
@@ -8,6 +8,16 @@ urlpatterns = [
         'article/<int:pk>',
         ArticleDetailView.as_view(),
         name='article_detail'
+    ),
+    path(
+        'article/create',
+         ArticleCreateView.as_view(),
+         name="article_create"
+    ),
+    path(
+        'article/<int:pk>/update',
+        ArticleUpdateView.as_view(),
+        name="article_update"
     ),
 ]
 
