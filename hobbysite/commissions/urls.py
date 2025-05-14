1a7aba9a6525085ece5ae0b74a6e5e2fc1e8ed8c
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-05-14 16:14:33 +0800

    Created new html template for update view, fixed urls

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
          CommissionListView.as_view(), name='commissions-list'),
      path('detail/<int:pk>', 
          CommissionDetailView.as_view(), name='commission-detail'),
-     path('create',
-          CommissionCreateView.as_view(), name='commission-create'),
+     path('add',
+          CommissionCreateView.as_view(), name='commission-add'),
      path('<int:pk>/edit',
-          CommissionUpdateView.as_view(), name ='commission-update')
+          CommissionUpdateView.as_view(), name ='commission-edit')
 ]
 
 
