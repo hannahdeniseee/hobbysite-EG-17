5908aefb135e8cf7d039befe0386389e74e26f70
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-05-15 22:57:17 +0800

    encrypted email host

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -164,5 +164,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'zandalee.labrador@student.ateneo.edu'
-EMAIL_HOST_PASSWORD = 'qnvy bdpy xmxo sjgb'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
