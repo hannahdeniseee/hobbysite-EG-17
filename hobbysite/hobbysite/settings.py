680ab0a98af06d8f784325e9ece27fa642e3342b
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-05-15 22:47:49 +0800

    changed password reset email handling

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -159,7 +159,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
