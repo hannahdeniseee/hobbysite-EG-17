7f6f4f6e015f7b9594e05c5e1baae1449cb659a8
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-05-17 21:43:35 +0800

    overwrite storage

--- hobbysite/wiki/apps.py
+++ hobbysite/wiki/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class WikiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wiki'
+    
+    def ready(self):
+        from django.conf import settings
+        settings.DEFAULT_FILE_STORAGE = 'hobbysite.storage_backends.MediaStorage'
