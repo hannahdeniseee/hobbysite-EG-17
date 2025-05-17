772b32e6e6104f09005c6585c6de84d50e4d1b4b
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-05-17 21:50:27 +0800

    overwrite storage

--- hobbysite/wiki/apps.py
+++ hobbysite/wiki/apps.py
@@ -1,4 +1,7 @@
 from django.apps import AppConfig
+from django.core.files.storage import default_storage
+from django.conf import settings
+from importlib import import_module
 
 
 class WikiConfig(AppConfig):
@@ -6,5 +9,10 @@ class WikiConfig(AppConfig):
     name = 'wiki'
     
     def ready(self):
-        from django.conf import settings
-        settings.DEFAULT_FILE_STORAGE = 'hobbysite.storage_backends.MediaStorage'
+        if hasattr(settings, 'DEFAULT_FILE_STORAGE'):
+            module_path, class_name = settings.DEFAULT_FILE_STORAGE.rsplit('.', 1)
+            storage_module = import_module(module_path)
+            storage_class = getattr(storage_module, class_name)
+            # Force Django to use your MediaStorage as the default
+            from django.core.files import storage as storage_module
+            storage_module.default_storage = storage_class()
